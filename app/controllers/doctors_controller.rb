905629fe3939a1804a0f624449badcf32f16decf
nextacademy <giinleong91@hotmail.com>
2018-03-09 18:43:19 +0800

    add doctor feature

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,23 @@
 class DoctorsController < ApplicationController
 
 def index
-  
+  @doctors = Doctor.all
+end
+
+def new
+  @doctor = Doctor.new  
+end
+
+def create
+  @doctor = Doctor.new(doctor_params)
+  if @doctor.save
+  else
+    render "new"
+  end
+end
+
+def edit
+  @doctor = Doctor.find(params[:id])
 end
 
 end
