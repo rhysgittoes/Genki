6474250d4644581c3c280fb01fe6b05bd5f27d1a
Rhys Gittoes <rhysgittoes@Rhyss-MacBook-Air.local>
2018-03-12 11:45:10 +0800

    Created t.date :date

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,23 @@
 
 ActiveRecord::Schema.define(version: 20180312032205) do
 
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "allergies", force: :cascade do |t|
+    t.string "type"
+    t.string "severity"
+    t.boolean "status"
+    t.date "status_update"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.text "symtoms", default: [], array: true
+    t.text "diagnosis"
+    t.string "referrals"
+    t.text "notes"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
