3986df84aa28867617a4b7e8476677cec844b0db
Emika Ijuin <emikaijuin@gmail.com>
2018-03-12 04:35:54 +0000

    Current schema based on final migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312041540) do
+ActiveRecord::Schema.define(version: 20180312043221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,10 +34,12 @@ ActiveRecord::Schema.define(version: 20180312041540) do
     t.text "diagnosis"
     t.string "referrals"
     t.text "notes"
-    t.bigint "doctor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.bigint "patient_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -117,7 +119,6 @@ ActiveRecord::Schema.define(version: 20180312041540) do
 
   add_foreign_key "allergies", "appointments"
   add_foreign_key "allergies", "patients"
-  add_foreign_key "appointments", "doctors"
   add_foreign_key "health_profiles", "patients"
   add_foreign_key "illnesses", "appointments"
   add_foreign_key "illnesses", "patients"
