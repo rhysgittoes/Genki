58121f8154c36a9fa550ed9bfad9cf05e80f704e
Emika Ijuin <emikaijuin@gmail.com>
2018-03-12 03:27:24 +0000

    Add timestamps to Patients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310151930) do
+ActiveRecord::Schema.define(version: 20180312032611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 20180310151930) do
     t.string "city"
     t.string "country"
     t.string "admission_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prescriptions", force: :cascade do |t|
+    t.string "medicine"
+    t.string "dosage"
+    t.string "refills"
+    t.date "expiration_date"
   end
 
 end
