6ed2e571e4e1567f86d59247ca1e11ad6400bf7c
Emika Ijuin <emikaijuin@gmail.com>
2018-03-12 09:09:21 +0000

    Comment out validations to expedite instance creations during development

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,18 +2,18 @@ class Doctor < ApplicationRecord
   has_many :patients
   has_many :prescriptions
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :gender, presence: true
-  validates :birthday, presence: true
-  validates :language, presence: true
-  validates :city, presence: true
-  validates :country, presence: true
-  validates :admission_id, presence: true
-  validates :certification, presence: true
-  validates :email, presence: true
-  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :experience, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :gender, presence: true
+  # validates :birthday, presence: true
+  # validates :language, presence: true
+  # validates :city, presence: true
+  # validates :country, presence: true
+  # validates :admission_id, presence: true
+  # validates :certification, presence: true
+  # validates :email, presence: true
+  # validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # validates :experience, presence: true
 
   scope :patient_name, ->(search) { where("first_name ILIKE :patient_name OR last_name ILIKE :patient_name", patient_name: "#{search}")}  
 
