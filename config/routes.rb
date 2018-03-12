a70e4eedb655faf4d3441122e81d6d5cf33d3efb
nextacademy <giinleong91@hotmail.com>
2018-03-12 18:35:40 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
 
 post "/doctors/search" => "doctors#search"
-=======
-  
-  get "/doctors/search" => "doctors#search"
->>>>>>> f4430608d40d7fd07e202830492dd8b9cb2dea57
+
   resources :doctors
   
   resources :appointments
   resources :patients
   resources :health_profiles
 
-<<<<<<< HEAD
 
-=======
->>>>>>> f4430608d40d7fd07e202830492dd8b9cb2dea57
   root to: 'pages#index'
 
 end
