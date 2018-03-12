a97b885a6289503317ab1b65e36e99f4ee12e728
Emika Ijuin <emikaijuin@gmail.com>
2018-03-12 09:08:47 +0000

    Fix search method and add basic logic to other methods

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,8 @@
 class DoctorsController < ApplicationController
+  
+  before_action :set_doctor, only: [:edit, :show, :update]
 
 def index
-  @doctors = Doctor.all
 end
 
 def new
@@ -9,14 +10,30 @@ def new
 end
 
 def create
-  @doctor = Doctor.new(doctor_params)
   if @doctor.save
+    redirect_to doctors_path
   else
-    render "new"
+    flash[:notice] = "Sorry, there was an error creating your account. Please try again."
+    redirect_to new_doctor_path
   end
 end
 
 def edit
+end
+
+def update
+end
+
+def search
+  @patients = Patient.where("first_name ILIKE ? OR last_name ILIKE ?", params[:search], params[:search])
+end
+
+def show
+end
+
+private
+
+def set_doctor
   @doctor = Doctor.find(params[:id])
 end
 
