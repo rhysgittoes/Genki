416b9dad92d15cb252e0ce2b0a98c085a050cf17
Rhys Gittoes <rhysgittoes@Rhyss-MacBook-Air.local>
2018-03-12 11:35:59 +0800

    Appointments table migration file created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309103709) do
+ActiveRecord::Schema.define(version: 20180312032205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.text "symtoms", default: [], array: true
+    t.text "diagnosis"
+    t.string "referrals"
+    t.text "notes"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
