ca1676fa7be318f8151e1f1b4f970e11ce074447
giinleong91 <35290374+giinleong91@users.noreply.github.com>
2018-03-13 09:57:19 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-post "/doctors/search" => "doctors#search"
+get "/doctors/search" => "doctors#search"
   
   resources :doctors
   
