90d7b525441324ed4b3b8be98030d3ae0221c505
Emika Ijuin <emikaijuin@gmail.com>
2018-03-13 04:22:34 +0000

    Remove prevent default on search bar

--- app/assets/javascripts/doctor.js
+++ app/assets/javascripts/doctor.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
 // jquery
-$("#search-form").keypress(function(e){
-  e.preventDefault();
+// $("#search-form").keypress(function(e){
+//   e.preventDefault();
   
-  $.ajax({
-    url: "/search",
-    method: "POST",
-    data: $(this).serialize(),
-    success: function(data){
-      data.forEach(function(element){
-          //create new option element
-          var option = document.createElement("option")
-          option.value = element
+//   $.ajax({
+//     url: "/search",
+//     method: "POST",
+//     data: $(this).serialize(),
+//     success: function(data){
+//       data.forEach(function(element){
+//           //create new option element
+//           var option = document.createElement("option")
+//           option.value = element
 
-          //append option to list
-          list.appendChild(option)
-        })
-    }
-  });
-})
-})
+//           //append option to list
+//           list.appendChild(option)
+//         })
+//     }
+//   });
+// })
+// })
