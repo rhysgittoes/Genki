a1ff8adca33b4567789b496b47235f709018e515
nextacademy <giinleong91@hotmail.com>
2018-03-14 18:18:57 +0800

    fix for reroute after user login

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,9 +1,4 @@
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "reply@example.com"
-  if Session.type == doctor 
-  config.redirect_url = "/doctors"
-  else 
-    config.redirect_url = "/patients"
-  end
 end
