307e2d8b43d564e0967ad8d43830af34784414a0
Emika Ijuin <emikaijuin@gmail.com>
2018-03-14 14:30:22 +0800

    This migration file is not needed

--- db/migrate/20180314051412_change_foreign_key_back_to_patient_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeForeignKeyBackToPatientId < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :health_profiles, :user_id, :bigint
-    add_reference :health_profiles, :patient, foreignkey: true
-  end
-end
