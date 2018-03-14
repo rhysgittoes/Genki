a81895b20087d059d49db91d9d44b89d171b3949
nextacademy <giinleong91@hotmail.com>
2018-03-14 18:52:09 +0800

    fix pages controller so @user == nil will show homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
   def index
    @user = current_user
-   if @user.type == "Doctor" 
-    redirect_to doctors_path
-  else
+   if @user == nil 
+    render "/pages/index"
+  elsif @user.type == "Patient"
     redirect_to patients_path
+  else @user.type == "Doctor"
+    redirect_to doctors_path
   end
   end
 end
