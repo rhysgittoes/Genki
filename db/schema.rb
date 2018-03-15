1a1bee76b1463624972a963c47757e5a8e53bcb8
Emika Ijuin <emikaijuin@gmail.com>
2018-03-15 10:45:57 +0000

    Create notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315055926) do
+ActiveRecord::Schema.define(version: 20180315090526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(version: 20180315055926) do
     t.date "expiration_date"
     t.string "name"
     t.index ["appointment_id"], name: "index_immunizations_on_appointment_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "text"
+    t.bigint "category"
+    t.bigint "user_type"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
