f636ad7599cf992d21549b62e5aed873cb228316
Emika Ijuin <emikaijuin@gmail.com>
2018-03-15 15:41:01 +0000

    Add read status column to notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315090526) do
+ActiveRecord::Schema.define(version: 20180315153216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20180315090526) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "received", default: false
   end
 
   create_table "patients", force: :cascade do |t|
