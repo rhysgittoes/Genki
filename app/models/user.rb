54d20275067e4f9fbb497566bff601f06ae54455
Emika Ijuin <emikaijuin@gmail.com>
2018-03-15 15:39:30 +0000

    User has many notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :notifications
-
+  
   def self.search(search)
     @patient = Patient.where("first_name ILIKE :search OR last_name ILIKE :search OR admission_id ILIKE :search ", search: "#{search}")
     @doctor = Doctor.where("first_name ILIKE :search OR last_name ILIKE :search OR admission_id ILIKE :search ", search: "#{search}") 
