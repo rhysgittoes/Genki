00aea280f083d4e56b135f5840436c4ffa3b68ac
Emika Ijuin <emikaijuin@gmail.com>
2018-03-16 06:28:36 +0000

    Add validation that associated models won't save if they are all blank

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,4 +3,13 @@ class Prescription < ApplicationRecord
 	belongs_to :doctor, class_name: "Doctor", foreign_key: "doctor_id"
 	belongs_to :appointment
 
+	validates :medicine, presence: true
+	validates :dosage, presence: true
+
+	before_save :set_default_values
+	
+	def set_default_values
+		self.refills = 0
+	end
+
 end
