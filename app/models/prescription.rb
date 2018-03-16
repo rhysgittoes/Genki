873fabbdc36429bffae7416afcf5b8ef752cebb3
Emika Ijuin <emikaijuin@gmail.com>
2018-03-16 07:28:52 +0000

    Trigger notifications to be created after prescriptions are saved

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -2,5 +2,11 @@ class Prescription < ApplicationRecord
 	belongs_to :patient, class_name: "Patient", foreign_key: "patient_id"
 	belongs_to :doctor, class_name: "Doctor", foreign_key: "doctor_id"
 	belongs_to :appointment
+	
+	after_save :create_notifications
+	
+	def create_notifications
+		Notification.create_new_prescription_notifications(self)
+	end
 
 end
