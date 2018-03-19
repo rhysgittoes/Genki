8d1c1c5cb43dc443bf06fd8302dc9a6da51c9000
Emika Ijuin <emikaijuin@gmail.com>
2018-03-19 16:40:57 +0000

    Belong to health_profile optionally & make appointment_id optional

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,7 +1,8 @@
 class Prescription < ApplicationRecord
 	belongs_to :patient, class_name: "Patient", foreign_key: "patient_id"
 	belongs_to :doctor, class_name: "Doctor", foreign_key: "doctor_id"
-	belongs_to :appointment
+	belongs_to :appointment, optional: true
+	belongs_to :health_profile, optional: true
   
 	validates :medicine, presence: true
 	validates :dosage, presence: true
