61b555259d8b07103eec6d74a0fa515b2917e8cd
Rhys Gittoes <rhys@gittoes.org>
2018-03-20 11:47:10 +0800

    Jscript edits

--- app/assets/javascripts/health_profile.js
+++ app/assets/javascripts/health_profile.js
@@ -1,3 +1,17 @@
 document.addEventListener('turbolinks:load', function(){
 
+   $("#new_apoint_button").click(function(event){
+      console.log("HewoHewo")
+   })
 });
+
+
+// $("#doctor_signup").click(function(event){
+//       event.preventDefault();
+//       if ($("#patient_hidden_form").css("display") == 'none') {
+//         $("#doctor_hidden_form").fadeToggle( "slow", "linear" );      
+//       } 
+//       else {
+//           $("#patient_hidden_form").css("display", "none"); 
+//           $("#doctor_hidden_form").fadeToggle( "slow", "linear" );
+//       }
