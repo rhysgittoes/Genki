268c6c1fb5ec119d06b4219504eec2e53ae02857
Rhys Gittoes <rhys@gittoes.org>
2018-03-20 12:12:45 +0800

    fixed form

--- app/assets/javascripts/health_profile.js
+++ app/assets/javascripts/health_profile.js
@@ -3,12 +3,12 @@ document.addEventListener('turbolinks:load', function(){
    $("#new_apoint_button").click(function(event){
       if($("#appoitments_form_HP").css("display") == "none"){
          $("#appoitments_list_HP").fadeToggle("slow","linear");
-         $("#appoitments_form_HP").fadeToggle("slow","linear");
+         $("#appoitments_form_HP").delay(700).fadeToggle("slow","linear");
          console.log("HELLO")
       }
       else{
          $("#appoitments_form_HP").fadeToggle("slow","linear");
-         $("#appoitments_list_HP").fadeToggle("slow","linear");
+         $("#appoitments_list_HP").delay(700).fadeToggle("slow","linear");
       }
 
 
